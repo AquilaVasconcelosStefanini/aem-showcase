ff44688e99ce781899a3a1dcc07be3ea029b7e29
Aquila Vasconcelos Da Silva <e_avsilva@stefanini.com>
2024-02-26 17:06:21 -0300

    Creating osgi config to retrieve user info without hardcoding it

---
+++ core/src/main/java/com/aem/showcase/core/configuration/comments/services/CommentsConfigService.java
@@ -0,0 +1,7 @@
+package com.aem.showcase.core.configuration.comments.services;
+
+public interface CommentsConfigService {
+    public String getSessionUserName();
+    
+    public String getSessionUserPass();
+}
